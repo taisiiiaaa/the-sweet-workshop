a256adbc225d88ae84e4a907e89a5c2146dc84d2
Evelina <evelina.kobyletska@gmail.com>
2026-08-15 09:43:14 +0200

    add changes js

--- src/js/bestsellers.js
+++ src/js/bestsellers.js
@@ -59,6 +59,7 @@ class BestsellersSlider {
     window.addEventListener("resize", this.handleResize.bind(this));
   }
 
+  // Вона бере масив десертів і перетворює кожен об'єкт на HTML-картку.
   renderCards() {
     this.track.innerHTML = this.data
       .map(
@@ -77,9 +78,9 @@ class BestsellersSlider {
           <div class="dessert-card__footer">
             <span class="dessert-card__price">${item.price} грн</span>
             <button class="dessert-card__btn" aria-label="Детальніше про ${item.name}">
-              <svg viewBox="0 0 24 24" fill="none" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                <path d="M5 12h14M12 5l7 7-7 7"/>
-              </svg>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M16.2902 7.801L6.69448 17.4028C6.52065 17.5726 6.32073 17.6575 6.09473 17.6575C5.8689 17.6575 5.67098 17.5706 5.50098 17.3968C5.32715 17.2228 5.24023 17.0228 5.24023 16.797C5.24023 16.5712 5.32715 16.3713 5.50098 16.1973L15.0967 6.60175H6.64773C6.4034 6.60175 6.1994 6.51942 6.03573 6.35475C5.87207 6.19025 5.79023 5.98717 5.79023 5.7455C5.79023 5.504 5.87207 5.30242 6.03573 5.14075C6.1994 4.97909 6.4034 4.89825 6.64773 4.89825H17.1477C17.3882 4.89825 17.5903 4.98017 17.754 5.144C17.9177 5.30767 17.9995 5.50967 17.9995 5.75V16.25C17.9995 16.4903 17.9172 16.6923 17.7527 16.856C17.5882 17.0198 17.3852 17.1018 17.1435 17.1018C16.8978 17.1018 16.6942 17.0198 16.5325 16.856C16.371 16.6923 16.2902 16.4903 16.2902 16.25V7.801Z" fill="#080C0C" />
+</svg>
             </button>
           </div>
         </div>
@@ -89,6 +90,7 @@ class BestsellersSlider {
       .join("");
   }
 
+  // Ця функція фактично рухає слайдер
   updateSlider() {
     this.cardsPerView = this.getCardsPerView();
     this.totalSlides = Math.max(0, this.data.length - this.cardsPerView);
@@ -108,6 +110,7 @@ class BestsellersSlider {
     this.updateButtons();
   }
 
+  // створення крапочок під слайдером
   renderDots() {
     const dotsCount = this.totalSlides + 1;
     this.dotsContainer.innerHTML = "";
@@ -189,9 +192,12 @@ class BestsellersSlider {
   }
 }
 
+// робота з api
 async function loadBestsellers() {
   try {
     const response = await fetch(API.products);
+    // перевірка демо
+    // const response = await fetch("https://test-error-url.com/products");
 
     if (!response.ok) {
       throw new Error(`Помилка HTTP: ${response.status}`);
